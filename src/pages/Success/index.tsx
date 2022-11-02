5acf2ae23a0c5e152c82ef6bcd68bba9f3e922e7
wonedd <vinmarv@hotmail.com>
2022-11-02 14:30:13 -0300

    sucess test

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,7 +1,19 @@
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+
 export default function Success() {
+    const { user } = useContext(AuthContext);
+    const { push } = useRouter();
     return (
         <div>
-            <h1>Te amo</h1>
+            <h1>Te amo {user.name}</h1>
+            <button type="button" onClick={() => push(`${user.githubUrl}`)}>
+                Github
+            </button>
+            <button type="button" onClick={() => push(`${user.linkedinUrl}`)}>
+                LinkedIn
+            </button>
         </div>
     );
 }

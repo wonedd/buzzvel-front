7e9abcfd80690b89da772f3af718b1137335649c
wonedd <vinmarv@hotmail.com>
2022-11-02 14:31:29 -0300

    sucess test

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -6,14 +6,19 @@ export default function Success() {
     const { user } = useContext(AuthContext);
     const { push } = useRouter();
     return (
-        <div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
             <h1>Te amo {user.name}</h1>
-            <button type="button" onClick={() => push(`${user.githubUrl}`)}>
-                Github
-            </button>
-            <button type="button" onClick={() => push(`${user.linkedinUrl}`)}>
-                LinkedIn
-            </button>
+            <div style={{ display: 'flex', gap: '8' }}>
+                <button type="button" onClick={() => push(`${user.githubUrl}`)}>
+                    Github
+                </button>
+                <button
+                    type="button"
+                    onClick={() => push(`${user.linkedinUrl}`)}
+                >
+                    LinkedIn
+                </button>
+            </div>
         </div>
     );
 }
